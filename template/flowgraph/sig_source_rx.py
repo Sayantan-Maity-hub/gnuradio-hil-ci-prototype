998cf8305057ec7795435ad2599d0aba02fc0019
Sayantan-Maity-hub <maitysayantan116@gmail.com>
2026-03-25 16:06:45 +0530

    update registry and sig_source_rx.py

--- template/flowgraph/sig_source_rx.py
+++ template/flowgraph/sig_source_rx.py
@@ -1,141 +1,44 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 
-#
-# SPDX-License-Identifier: GPL-3.0
-#
-# GNU Radio Python Flow Graph
-# Title: Not titled yet
-# Author: maity
-# GNU Radio version: 3.10.12.0
-
-from PyQt5 import Qt
-from gnuradio import qtgui
-from gnuradio import blocks
-from gnuradio import gr
-from gnuradio.filter import firdes
-from gnuradio.fft import window
-import sys
-import signal
-from PyQt5 import Qt
+from gnuradio import gr, blocks, uhd
 from argparse import ArgumentParser
-from gnuradio.eng_arg import eng_float, intx
-from gnuradio import eng_notation
-from gnuradio import uhd
 import time
-import threading
 
+class RX(gr.top_block):
+    def __init__(self, samp_rate, output_file):
+        gr.top_block.__init__(self, "rx")
 
-
-class sig_source_rx(gr.top_block, Qt.QWidget):
-
-    def __init__(self):
-        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("Not titled yet")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except BaseException as exc:
-            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "sig_source_rx")
-
-        try:
-            geometry = self.settings.value("geometry")
-            if geometry:
-                self.restoreGeometry(geometry)
-        except BaseException as exc:
-            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
-        self.flowgraph_started = threading.Event()
-
-        ##################################################
-        # Variables
-        ##################################################
-        self.samp_rate = samp_rate = 32000
-
-        ##################################################
-        # Blocks
-        ##################################################
-
-        self.uhd_usrp_source_0 = uhd.usrp_source(
-            ",".join(("", '')),
-            uhd.stream_args(
-                cpu_format="fc32",
-                args='',
-                channels=list(range(0,1)),
-            ),
+        self.src = uhd.usrp_source(
+            ",".join(("", "")),
+            uhd.stream_args(cpu_format="fc32", channels=[0]),
         )
-        self.uhd_usrp_source_0.set_samp_rate(samp_rate)
-        self.uhd_usrp_source_0.set_time_unknown_pps(uhd.time_spec(0))
 
-        self.uhd_usrp_source_0.set_center_freq(0, 0)
-        self.uhd_usrp_source_0.set_antenna("RX2", 0)
-        self.uhd_usrp_source_0.set_gain(0, 0)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_gr_complex*1, '', False)
-        self.blocks_file_sink_0.set_unbuffered(False)
+        self.src.set_samp_rate(samp_rate)
+        self.src.set_center_freq(0, 0)
+        self.src.set_gain(0, 0)
 
+        self.sink = blocks.file_sink(
+            gr.sizeof_gr_complex,
+            output_file
+        )
 
-        ##################################################
-        # Connections
-        ##################################################
-        self.connect((self.uhd_usrp_source_0, 0), (self.blocks_file_sink_0, 0))
+        self.connect(self.src, self.sink)
 
 
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("gnuradio/flowgraphs", "sig_source_rx")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--duration", type=int, default=10)
+    parser.add_argument("--output", type=str, default="rx_output.dat")
 
-        event.accept()
+    args = parser.parse_args()
 
-    def get_samp_rate(self):
-        return self.samp_rate
-
-    def set_samp_rate(self, samp_rate):
-        self.samp_rate = samp_rate
-        self.uhd_usrp_source_0.set_samp_rate(self.samp_rate)
-
-
-
-
-def main(top_block_cls=sig_source_rx, options=None):
-
-    qapp = Qt.QApplication(sys.argv)
-
-    tb = top_block_cls()
+    tb = RX(32000, args.output)
 
     tb.start()
-    tb.flowgraph_started.set()
+    time.sleep(args.duration)
+    tb.stop()
+    tb.wait()
 
-    tb.show()
 
-    def sig_handler(sig=None, frame=None):
-        tb.stop()
-        tb.wait()
-
-        Qt.QApplication.quit()
-
-    signal.signal(signal.SIGINT, sig_handler)
-    signal.signal(signal.SIGTERM, sig_handler)
-
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
-
-    qapp.exec_()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
